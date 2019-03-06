d72e89ed8f02f5d796afa2274cb7162f2086ddb1
nalia3486 <nalia3486@gmail.com>
2019-03-06 20:15:16 +0100

    convert image to pdf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,77 +1,27 @@
-import java.awt.EventQueue;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
 import java.util.ArrayList;
-import java.util.Vector;
 
-import static javafx.application.Application.launch;
-import static javafx.application.Application.launch;
-
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
-import javafx.stage.Stage;
-
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import org.opencv.core.Core;
-
-import static org.opencv.core.Core.BORDER_DEFAULT;
-
-import org.opencv.core.Core.MinMaxLocResult;
-
-import static org.opencv.core.Core.NORM_MINMAX;
-import static org.opencv.core.Core.addWeighted;
-import static org.opencv.core.Core.flip;
-import static org.opencv.core.Core.minMaxLoc;
-import static org.opencv.core.Core.normalize;
-import static org.opencv.core.Core.randu;
-import static org.opencv.core.Core.transpose;
-
-import org.opencv.core.CvType;
-
-import static org.opencv.core.CvType.CV_64F;
-import static org.opencv.core.CvType.CV_8U;
-import static org.opencv.core.CvType.CV_8UC1;
-import static org.opencv.core.CvType.CV_8UC3;
-
 import org.opencv.core.Mat;
 
-import static org.opencv.core.Mat.zeros;
-
-import org.opencv.core.MatOfFloat;
-import org.opencv.core.MatOfInt;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Scalar;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 
-import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
-import static org.opencv.imgcodecs.Imgcodecs.imwrite;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
 
-import org.opencv.imgproc.Imgproc;
+import java.io.FileOutputStream;
+import java.util.List;
 
-import static org.opencv.imgproc.Imgproc.MORPH_OPEN;
-import static org.opencv.imgproc.Imgproc.filter2D;
-import static org.opencv.imgproc.Imgproc.line;
-import static org.opencv.imgproc.Imgproc.rectangle;
 
 public class Main {
     static {
@@ -79,8 +29,42 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        readImage("cat.jpg");
-        readImage("lenka.png");
+        //readImage("cat.jpg");
+        //readImage("lenka.png");
+
+        try {
+            imageToPDF("test.jpg");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void imageToPDF(String filename) throws Exception {
+        File root = new File("..\\2019-Image-Editor-OpenCV");
+
+        String outputFile = "some.pdf";
+
+        //to na wypadek, gdyby mialo byc wiecej zdjec w jednym pdfie
+        List<String> files = new ArrayList<>();
+        files.add(filename);
+
+        Document document = new Document(PageSize.A4, 10, 10, 10, 10);
+
+        PdfWriter.getInstance(document, new FileOutputStream(new File(root, outputFile)));
+        document.open();
+
+        for (String f : files) {
+            document.newPage();
+            com.itextpdf.text.Image image = com.itextpdf.text.Image.getInstance(new File(root, f).getAbsolutePath());
+            if (image.getHeight() > (document.getPageSize().getHeight() - 20)
+                    || image.getScaledWidth() > (document.getPageSize().getWidth() - 20)) {
+                image.scaleToFit(document.getPageSize().getWidth() - 20, document.getPageSize().getHeight() - 20);
+            }
+
+            document.add(image);
+        }
+
+        document.close();
     }
 
     private static void readImage(String filename) {
@@ -94,7 +78,7 @@ public class Main {
         }
     }
 
-    public static BufferedImage Mat2BufferedImage(Mat m) {
+    private static BufferedImage Mat2BufferedImage(Mat m) {
         int type = BufferedImage.TYPE_BYTE_GRAY;
         if (m.channels() > 1) {
             type = BufferedImage.TYPE_3BYTE_BGR;
@@ -108,7 +92,7 @@ public class Main {
         return image;
     }
 
-    public static void displayImage(Image img) {
+    private static void displayImage(Image img) {
         ImageIcon icon = new ImageIcon(img);
         JFrame frame = new JFrame();
         frame.setLayout(new FlowLayout());
